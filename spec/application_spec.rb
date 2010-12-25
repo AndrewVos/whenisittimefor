1c395bd5d1d347ab6945861d8553437b65c19e02
Andrew Vos <andrew.vos@gmail.com>
2010-12-25 16:10:45 +0000

    test that the response is rendered

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -1,9 +1,4 @@
-ENV['RACK_ENV'] = 'test'
-
-require 'application'
-require 'rspec'
-require 'rack/test'
-require 'date'
+require File.join(File.dirname(__FILE__), 'spec_helper.rb')
 
 describe Application do
   include Rack::Test::Methods
@@ -11,24 +6,23 @@ describe Application do
   def app
     Application
   end
-  
+
   context "GET /" do
     it "should not 404" do
       get "/"
       last_response.ok?.should == true
     end
-    
-    it "should show the time left formatted" do
-      #14th june 2011
-      DateTime.stub!(:now).and_return(DateTime.new(2009, 10, 10))
-      end_date = DateTime.new(2011, 6, 14)
 
-      get '/'
+     it "should show the time left formatted" do
+       DateTime.stub!(:now).and_return(DateTime.new(2009, 10, 10))
+       end_date = DateTime.new(2011, 6, 14)
 
-      app.new do |erb_app|
-        expected_response_body = erb_app.erb(:index)
-        last_response.body.should include "5 minutes and 20 seconds"
-      end
-    end
+       get '/'
+
+       app.new do |erb_app|
+         expected_response_body = erb_app.erb(:index)
+         last_response.body.should include "5 minutes and 20 seconds"
+       end
+     end
   end
 end
